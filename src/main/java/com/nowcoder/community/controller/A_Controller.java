961daac5cb08a42b29029902ff979ffc0c74a0c3
Luojianyu <864861987@qq.com>
2022-04-10 10:31:25 +0800

    Session测试

--- src/main/java/com/nowcoder/community/controller/A_Controller.java
+++ src/main/java/com/nowcoder/community/controller/A_Controller.java
@@ -1,6 +1,7 @@
 package com.nowcoder.community.controller;
 
 import com.nowcoder.community.service.a_Service;
+import com.nowcoder.community.util.CommunityUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -8,9 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.ServletOutputStream;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.*;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.*;
@@ -24,23 +23,23 @@ public class A_Controller {
 
 	@RequestMapping("/hello")
 	@ResponseBody
-	public String say(){
+	public String say() {
 		return "Hello Offer.";
 	}
 
 	@RequestMapping("/data")
 	@ResponseBody
-	public String getData(){
+	public String getData() {
 		return a_service.find();
 	}
 
 	@RequestMapping("/http")
-	public void http(HttpServletRequest request, HttpServletResponse response){
+	public void http(HttpServletRequest request, HttpServletResponse response) {
 		//获取请求数据
 		System.out.println(request.getMethod());
 		System.out.println(request.getServletPath());
 		Enumeration<String> enumeration = request.getHeaderNames();
-		while(enumeration.hasMoreElements()){
+		while (enumeration.hasMoreElements()) {
 			String name = enumeration.nextElement();
 			String value = request.getHeader(name);
 			System.out.println(name + ":" + value);
@@ -49,7 +48,7 @@ public class A_Controller {
 
 		//返回相应数据
 		response.setContentType("text/html;charset=utf-8");
-		try(
+		try (
 				PrintWriter writer = response.getWriter();
 		) {
 
@@ -62,12 +61,12 @@ public class A_Controller {
 	//GET请求
 
 	// /students?current=1&limit=20
-	@RequestMapping(path="/students", method = RequestMethod.GET)
+	@RequestMapping(path = "/students", method = RequestMethod.GET)
 	@ResponseBody
 	public String getStudents(
 			@RequestParam(name = "current", required = false, defaultValue = "1") int current,
 			@RequestParam(name = "limit", required = false, defaultValue = "20") int limit
-	){
+	) {
 		System.out.println(current);
 		System.out.println(limit);
 		return "some students";
@@ -76,7 +75,7 @@ public class A_Controller {
 	// student/123
 	@RequestMapping(path = "student/{id}", method = RequestMethod.GET)
 	@ResponseBody
-	public String getStudent(@PathVariable("id") int id){
+	public String getStudent(@PathVariable("id") int id) {
 		System.out.println(id);
 		return "a student";
 	}
@@ -84,7 +83,7 @@ public class A_Controller {
 	//POST请求
 	@RequestMapping(path = "/student", method = RequestMethod.POST)
 	@ResponseBody
-	public String saveStudent(String name, int age){
+	public String saveStudent(String name, int age) {
 		System.out.println(name);
 		System.out.println(age);
 		return "success";
@@ -93,16 +92,16 @@ public class A_Controller {
 	//相应HTML数据
 
 	@RequestMapping(path = "/teacher", method = RequestMethod.GET)
-	public ModelAndView getTeacher(){
+	public ModelAndView getTeacher() {
 		ModelAndView mav = new ModelAndView();
-		mav.addObject("name","张三");
+		mav.addObject("name", "张三");
 		mav.addObject("age", "20");
 		mav.setViewName("/demo/view");
 		return mav;
 	}
 
 	@RequestMapping(path = "/school", method = RequestMethod.GET)
-	public String getSchool(Model model){
+	public String getSchool(Model model) {
 		model.addAttribute("name", "大连理工大学");
 		model.addAttribute("age", 73);
 		return "/demo/view";
@@ -113,7 +112,7 @@ public class A_Controller {
 
 	@RequestMapping(path = "/emp", method = RequestMethod.GET)
 	@ResponseBody
-	public Map<String, Object> getEmp(){
+	public Map<String, Object> getEmp() {
 		Map<String, Object> emp = new HashMap<>();
 		emp.put("name", "张三");
 		emp.put("age", 23);
@@ -123,7 +122,7 @@ public class A_Controller {
 
 	@RequestMapping(path = "/emps", method = RequestMethod.GET)
 	@ResponseBody
-	public List<Map<String, Object>> getEmps(){
+	public List<Map<String, Object>> getEmps() {
 		List<Map<String, Object>> list = new ArrayList<>();
 		Map<String, Object> emp = new HashMap<>();
 		emp.put("name", "张三");
@@ -142,6 +141,45 @@ public class A_Controller {
 		list.add(emp);
 
 		return list;
+	}
+
+	//cookie示例
+	@RequestMapping(path = "/cookie/set", method = RequestMethod.GET)
+	@ResponseBody
+	public String setCookie(HttpServletResponse response) {
+		//创建cookie
+		Cookie cookie = new Cookie("code", CommunityUtil.generateUUID());
+		//生效范围
+		cookie.setPath("/community/a");
+		//设置cookie的生存时间
+		cookie.setMaxAge(60 * 10);
+		//发送cookie
+		response.addCookie(cookie);
+		return "set cookie";
+	}
+
+	@RequestMapping(path = "/cookie/get", method = RequestMethod.GET)
+	@ResponseBody
+	public String getCookie(@CookieValue("code") String code, HttpServletRequest request) {
+		System.out.println(code);
+		return "get cookie";
+	}
+
+	//session示例
+	@RequestMapping(path = "/session/set", method = RequestMethod.GET)
+	@ResponseBody
+	public String setSession(HttpSession session) {
+		session.setAttribute("id", 1);
+		session.setAttribute("name", "Session");
+		return "set session";
+	}
+
+	@RequestMapping(path = "/session/get", method = RequestMethod.GET)
+	@ResponseBody
+	public String getSession(HttpSession session) {
+		System.out.println(session.getAttribute("id"));
+		System.out.println(session.getAttribute("name"));
+		return "get session";
 	}
 
 }
